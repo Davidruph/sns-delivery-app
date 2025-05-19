de8c3781bcc9c299d70f7d0ab9cf610af132a49e
Agbugba David C. <57929456+Davidruph@users.noreply.github.com>
2025-05-19 16:35:50 +0100

    change switcher

--- public/js/styleSwitcher.js
+++ public/js/styleSwitcher.js
@@ -22,48 +22,6 @@ function addSwitcher() {
     }
 }
 
-// Set a cookie
-function setCookie(name, value, days = 30) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-// Get a cookie
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === " ") c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0)
-            return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-// Delete a specific cookie
-function eraseCookie(name) {
-    document.cookie = name + "=; Max-Age=-99999999;";
-}
-
-// Delete all cookies (used in "Delete All Cookie" button)
-function deleteAllCookie() {
-    document.cookie.split(";").forEach(function (c) {
-        document.cookie = c
-            .replace(/^ +/, "")
-            .replace(
-                /=.*/,
-                "=;expires=" + new Date().toUTCString() + ";path=/",
-            );
-    });
-    location.reload(); // Optional: reload page after clearing cookies
-}
-
 (function ($) {
     "use strict";
     addSwitcher();
